a729fbd14cf58e10e3ef5d6c7925d7f0cb6bebbc
nazo6 <git@nazo6.dev>
2024-12-01 01:40:03 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,38 @@
 #![feature(impl_trait_in_assoc_type)]
 
 use core::panic::PanicInfo;
+use core::ptr::addr_of_mut;
 
 use embassy_executor::Spawner;
 use embassy_nrf::{
+    bind_interrupts,
     gpio::{Input, OutputDrive, Pull},
     interrupt::{self, InterruptExt, Priority},
-    peripherals::SPI2,
+    peripherals::{SPI2, USBD},
+    usb::vbus_detect::SoftwareVbusDetect,
 };
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
-use hooks::NegBacklightHooks;
+use embedded_alloc::LlffHeap as Heap;
 use misc::PAW3395_CONFIG;
 use once_cell::sync::OnceCell;
-
 use rktk::{
-    hooks::EmptyMainHooks,
-    interface::{
-        debounce::EagerDebounceDriver, double_tap::DummyDoubleTapResetDriver,
-        split::DummySplitDriver, storage::DummyStorageDriver,
-    },
-    task::Drivers,
+    drivers::Drivers, hooks::EmptyMainHooks, interface::debounce::EagerDebounceDriver, none_driver,
 };
 use rktk_drivers_common::encoder::GeneralEncoder;
-use rktk_drivers_nrf::panic_utils;
 use rktk_drivers_nrf::{
-    backlight::ws2812_pwm::Ws2812Pwm, display::ssd1306::create_ssd1306,
-    keyscan::shift_register_matrix::create_shift_register_matrix, mouse::paw3395,
-    softdevice::flash::get_flash, usb::UsbOpts,
+    backlight::ws2812_pwm::Ws2812Pwm,
+    display::ssd1306::create_ssd1306,
+    keyscan::shift_register_matrix::create_shift_register_matrix,
+    mouse::paw3395,
+    panic_utils,
+    softdevice::{
+        ble::{init_ble_server, NrfBleDriverBuilder},
+        flash::get_flash,
+    },
+    usb::{new_usb, Config as UsbConfig, UsbOpts},
 };
+
+use hooks::NegBacklightHooks;
 
 use nrf_softdevice as _;
 
@@ -37,34 +42,7 @@ mod hooks;
 mod keymap;
 mod misc;
 
-#[cfg(feature = "ble")]
-mod ble {
-    pub use rktk_drivers_nrf::softdevice::ble::init_ble_server;
-    pub use rktk_drivers_nrf::softdevice::ble::NrfBleDriverBuilder;
-}
-
-#[cfg(not(feature = "ble"))]
-mod no_ble {
-    pub use rktk::interface::ble::DummyBleDriverBuilder;
-}
-
-#[cfg(feature = "usb")]
-mod usb {
-    pub use embassy_nrf::usb::vbus_detect::SoftwareVbusDetect;
-    pub use rktk_drivers_nrf::usb::{new_usb, Config as UsbConfig};
-}
-
-#[cfg(not(feature = "usb"))]
-mod no_usb {
-    pub use rktk::interface::usb::DummyUsbDriverBuilder;
-}
-
-use embassy_nrf::{bind_interrupts, peripherals::USBD};
-
 extern crate alloc;
-
-use core::ptr::addr_of_mut;
-use embedded_alloc::LlffHeap as Heap;
 
 #[global_allocator]
 static HEAP: Heap = Heap::empty();
@@ -76,11 +54,7 @@ bind_interrupts!(pub struct Irqs {
     UARTE0_UART0 => embassy_nrf::buffered_uarte::InterruptHandler<embassy_nrf::peripherals::UARTE0>;
 });
 
-static SOFTWARE_VBUS: OnceCell<usb::SoftwareVbusDetect> = OnceCell::new();
-
-pub fn map_key(row: usize, col: usize) -> Option<(usize, usize)> {
-    Some((row, col))
-}
+static SOFTWARE_VBUS: OnceCell<SoftwareVbusDetect> = OnceCell::new();
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
@@ -91,29 +65,17 @@ async fn main(_spawner: Spawner) {
         unsafe { HEAP.init(addr_of_mut!(HEAP_MEM) as usize, HEAP_SIZE) }
     }
 
-    // - About limitation of softdevice
-    // By enabling softdevice, some interrupt priority level (P0,P1,P4)
-    // and peripherals are reserved by softdevice, and using them causes panic.
-    //
-    // Example reserved peripherals are:
-    // - TIMER0
-    // - CLOCK
-    // - RTC0
-    // ... and more
-    //
-    // ref:
-    // List of reserved peripherals: https://docs.nordicsemi.com/bundle/sds_s140/page/SDS/s1xx/sd_resource_reqs/hw_block_interrupt_vector.html
-    // Peripheral register addresses: https://docs.nordicsemi.com/bundle/ps_nrf52840/page/memory.html
-    //
-    // When panic occurs by peripheral conflict, PC address that caused panic is logged.
-    // By investigating the address using decompiler tools like ghidra, you can find the peripheral that caused the panic.
-
-    let mut config = embassy_nrf::config::Config::default();
-    config.gpiote_interrupt_priority = Priority::P2;
-    config.time_interrupt_priority = Priority::P2;
-    config.lfclk_source = embassy_nrf::config::LfclkSource::ExternalXtal;
-    config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
-    let p = embassy_nrf::init(config);
+    let p = {
+        let config = {
+            let mut config = embassy_nrf::config::Config::default();
+            config.gpiote_interrupt_priority = Priority::P2;
+            config.time_interrupt_priority = Priority::P2;
+            config.lfclk_source = embassy_nrf::config::LfclkSource::ExternalXtal;
+            config.hfclk_source = embassy_nrf::config::HfclkSource::ExternalXtal;
+            config
+        };
+        embassy_nrf::init(config)
+    };
 
     let led_cutoff = embassy_nrf::gpio::Output::new(
         p.P0_31,
@@ -126,27 +88,27 @@ async fn main(_spawner: Spawner) {
     interrupt::SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0.set_priority(Priority::P2);
     interrupt::UARTE1.set_priority(Priority::P2);
 
-    let display = create_ssd1306(
+    let Some(display) = panic_utils::display_message_if_panicked(create_ssd1306(
         p.TWISPI0,
         Irqs,
         p.P1_00,
         p.P0_11,
         ssd1306::size::DisplaySize128x32,
-    );
-
-    let Some(display) = panic_utils::display_message_if_panicked(display).await else {
+    ))
+    .await
+    else {
         cortex_m::asm::udf()
     };
 
-    let mut spi_config = paw3395::recommended_paw3395_config();
-    spi_config.sck_drive = OutputDrive::Standard;
-    spi_config.mosi_drive = OutputDrive::Standard;
-    spi_config.frequency = embassy_nrf::spim::Frequency::K250;
-    let shared_spi = Mutex::<CriticalSectionRawMutex, _>::new(embassy_nrf::spim::Spim::new(
-        p.SPI2, Irqs, p.P0_17, p.P0_22, p.P0_20, spi_config,
-    ));
-
-    // let ball = pmw3360::create_pmw3360(&shared_spi, p.P1_06);
+    let shared_spi = {
+        let mut spi_config = paw3395::recommended_paw3395_config();
+        spi_config.sck_drive = OutputDrive::Standard;
+        spi_config.mosi_drive = OutputDrive::Standard;
+        spi_config.frequency = embassy_nrf::spim::Frequency::K250;
+        Mutex::<CriticalSectionRawMutex, _>::new(embassy_nrf::spim::Spim::new(
+            p.SPI2, Irqs, p.P0_17, p.P0_22, p.P0_20, spi_config,
+        ))
+    };
 
     let ball = paw3395::create_paw3395(&shared_spi, p.P1_06, PAW3395_CONFIG);
 
@@ -170,11 +132,32 @@ async fn main(_spawner: Spawner) {
     )]);
 
     let backlight = Ws2812Pwm::new(p.PWM0, p.P0_24);
+    let usb = {
+        let vbus = SOFTWARE_VBUS.get_or_init(|| SoftwareVbusDetect::new(true, true));
+        let driver = embassy_nrf::usb::Driver::new(p.USBD, Irqs, vbus);
+        let opts = UsbOpts {
+            config: {
+                let mut config = UsbConfig::new(0xc0de, 0xcafe);
+
+                config.manufacturer = Some("nazo6");
+                config.product = Some("negL");
+                config.serial_number = Some("12345678");
+                config.max_power = 100;
+                config.max_packet_size_0 = 64;
+                config.supports_remote_wakeup = true;
+
+                config
+            },
+            mouse_poll_interval: 2,
+            kb_poll_interval: 5,
+            driver,
+        };
+        Some(new_usb(opts))
+    };
 
     let sd = rktk_drivers_nrf::softdevice::init_sd("negL");
 
-    #[cfg(feature = "ble")]
-    let server = ble::init_ble_server(
+    let server = init_ble_server(
         sd,
         rktk_drivers_nrf::softdevice::ble::DeviceInformation {
             manufacturer_name: Some("nazo6"),
@@ -186,64 +169,26 @@ async fn main(_spawner: Spawner) {
     .await;
 
     rktk_drivers_nrf::softdevice::start_softdevice(sd).await;
-
     embassy_time::Timer::after_millis(50).await;
-
     // let rand = rktk_drivers_nrf52::softdevice::rand::SdRand::new(sd);
-
     let (flash, cache) = get_flash(sd);
     let storage = rktk_drivers_nrf::softdevice::flash::create_storage_driver(flash, &cache);
 
-    let ble_builder = {
-        #[cfg(feature = "ble")]
-        let ble = Some(ble::NrfBleDriverBuilder::new(sd, server, "negL", flash).await);
-
-        #[cfg(not(feature = "ble"))]
-        let ble = Option::<no_ble::DummyBleDriverBuilder>::None;
-
-        ble
-    };
+    let ble_builder = Some(NrfBleDriverBuilder::new(sd, server, "negL", flash).await);
 
     let drivers = Drivers {
         keyscan,
-        double_tap_reset: Option::<DummyDoubleTapResetDriver>::None,
+        double_tap_reset: none_driver!(DoubleTapReset),
         mouse_builder: Some(ball),
-        usb_builder: {
-            #[cfg(feature = "usb")]
-            let usb = {
-                let vbus = SOFTWARE_VBUS.get_or_init(|| usb::SoftwareVbusDetect::new(true, true));
-                let driver = embassy_nrf::usb::Driver::new(p.USBD, Irqs, vbus);
-                let opts = UsbOpts {
-                    config: {
-                        let mut config = usb::UsbConfig::new(0xc0de, 0xcafe);
-
-                        config.manufacturer = Some("nazo6");
-                        config.product = Some("negL");
-                        config.serial_number = Some("12345678");
-                        config.max_power = 100;
-                        config.max_packet_size_0 = 64;
-                        config.supports_remote_wakeup = true;
-
-                        config
-                    },
-                    mouse_poll_interval: 2,
-                    kb_poll_interval: 5,
-                    driver,
-                };
-                Some(usb::new_usb(opts))
-            };
-
-            #[cfg(not(feature = "usb"))]
-            let usb = Option::<no_usb::DummyUsbDriverBuilder>::None;
-
-            usb
-        },
+        usb_builder: usb,
         display_builder: Some(display),
-        split: DummySplitDriver,
+        split: none_driver!(Split),
         backlight: Some(backlight),
-        storage: Option::<DummyStorageDriver>::None,
+        storage: Some(storage),
         ble_builder,
-        debounce: EagerDebounceDriver::new(embassy_time::Duration::from_millis(20)),
+        debounce: Some(EagerDebounceDriver::new(
+            embassy_time::Duration::from_millis(20),
+        )),
         encoder: Some(encoder),
     };
 
