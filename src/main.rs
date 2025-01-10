199abdf6a98c5b2a4c089692c5cb91ca38b83f65
nazo6 <git@nazo6.dev>
2025-01-10 16:09:53 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -50,9 +50,9 @@ mod misc;
 
 bind_interrupts!(pub struct Irqs {
     USBD => embassy_nrf::usb::InterruptHandler<USBD>;
-    SPIM2_SPIS2_SPI2 => embassy_nrf::spim::InterruptHandler<SPI2>;
-    SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0 => embassy_nrf::twim::InterruptHandler<embassy_nrf::peripherals::TWISPI0>;
-    UARTE0_UART0 => embassy_nrf::buffered_uarte::InterruptHandler<embassy_nrf::peripherals::UARTE0>;
+    SPI2 => embassy_nrf::spim::InterruptHandler<SPI2>;
+    TWISPI0 => embassy_nrf::twim::InterruptHandler<embassy_nrf::peripherals::TWISPI0>;
+    UARTE0 => embassy_nrf::buffered_uarte::InterruptHandler<embassy_nrf::peripherals::UARTE0>;
 });
 
 static SOFTWARE_VBUS: OnceCell<SoftwareVbusDetect> = OnceCell::new();
@@ -71,9 +71,9 @@ fn init() -> Peripherals {
     };
 
     interrupt::USBD.set_priority(Priority::P2);
-    interrupt::SPIM2_SPIS2_SPI2.set_priority(Priority::P2);
-    interrupt::SPIM0_SPIS0_TWIM0_TWIS0_SPI0_TWI0.set_priority(Priority::P2);
-    interrupt::UARTE1.set_priority(Priority::P2);
+    interrupt::SPI2.set_priority(Priority::P2);
+    interrupt::SPIM3.set_priority(Priority::P2);
+    interrupt::UARTE0.set_priority(Priority::P2);
 
     p
 }
